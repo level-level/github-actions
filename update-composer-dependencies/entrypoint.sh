27f63d2de21cc2e6386d4211d22dcbe36961ea3a
Niels de Blaauw <niels@level-level.com>
2019-08-21 15:41:55 +0200

    Adds call to create pull request

--- update-composer-dependencies/entrypoint.sh
+++ update-composer-dependencies/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -l
 
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+URI=https://api.github.com
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
 echo "Starting update sequence."
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git config --global user.email "bot-tank-top+al-dente@level-level.com"
@@ -21,3 +32,11 @@ git remote add origin "${remote_repo}"
 
 echo "Done. Pushing branch.";
 git push -u origin auto-update/$TIMESTAMP
+
+echo "Opening pull request.";
+curl -XPOST -fsSL \
+    -H "${AUTH_HEADER}" \
+    -H "${API_HEADER}" \
+    -H "Content-Type: application/json" \
+    -d "{\"head\":\"auto-update/$TIMESTAMP\", \"base\":\"master\", \"title\":\"Plugin updates\", \"body\":\"Minor updates automatically created.\"}" \
+    "${URI}/repos/${GITHUB_REPOSITORY}/pulls"
